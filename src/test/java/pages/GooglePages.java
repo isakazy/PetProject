6cf329fe8e0d85ca5df44e7a7f36b2128a14df7e
Isa Amanbaev <isaamanbaev@Isas-MacBook-Air.local>
2024-06-12 13:42:01 -0500

    pom xml that includes the plugin modification

--- src/test/java/pages/GooglePages.java
+++ src/test/java/pages/GooglePages.java
@@ -1,6 +1,5 @@
 package pages;
 
-import io.cucumber.java.zh_cn.假如;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
